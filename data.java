9df8b0e9c746929ae5f73c28271f178a085bc62e
BohdanProdanyk <126155459+BohdanProdanyk@users.noreply.github.com>
2023-04-26 00:08:05 +0300

    Update data.java

--- data.java
+++ data.java
@@ -1,13 +1,18 @@
-public class data {
+public class Data {
     int floor;
     int room;
     double area;
     String parking;
 
-    public data(int floor, int room, double area, String parking){
+
+    public Data(int floor, int room, double area, String parking){
         this.floor = floor;
         this.room = room;
         this.area = area;
         this.parking = parking;
+    }
+    void ploshcha_one(){
+        double q = (area / room);
+        System.out.println(q);
     }
 }
