4b43bf4e75bf2370866e9ee4f6dcf16b00dabf4a
BohdanProdanyk <126155459+BohdanProdanyk@users.noreply.github.com>
2023-04-26 00:09:07 +0300

    Update furniture.java

--- furniture.java
+++ furniture.java
@@ -1,16 +1,29 @@
-public class furniture {
+public class Furniture {
     String kitchen;
+    int kit;
     String badroom;
+    int bad;
     String toilet;
+    int toi;
     String living_room;
+    int liv;
     String wardrobe;
+    int war;
 
-    public furniture(String kitchen, String badroom, String toilet, String living_room,  String wardrobe){
+    public Furniture(String kitchen, int kit, String badroom, int bad, String toilet, int toi, String living_room, int liv, String wardrobe){
         this.kitchen = kitchen;
         this.badroom = badroom;
         this.toilet = toilet;
         this.living_room = living_room;
         this.wardrobe = wardrobe;
+        this.kit = kit;
+        this.bad = bad;
+        this.toi = toi;
+        this.liv = liv;
+    }
+    void sum(){
+        int m = kit + bad + toi + liv;
+        System.out.println(m);
     }
 
 }
